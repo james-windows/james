6e4faaf5ce65fe760ad32051ce5c17e44feab1ca
Michael Moser <michaelmoser1996@gmail.com>
2015-08-31 11:10:17 +0200

    Added version number to About section
    Prepared the space for displaying the actual version number of the running
    application. Right version number will be implemented soon.

--- WinFred/UserControls/AboutUserControl.xaml.cs
+++ WinFred/UserControls/AboutUserControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
+using Squirrel;
 
 namespace WinFred.UserControls
 {
@@ -7,9 +9,18 @@ namespace WinFred.UserControls
     /// </summary>
     public partial class AboutUserControl : UserControl
     {
+        public string Version { get; private set; }
         public AboutUserControl()
         {
             InitializeComponent();
+            #if DEBUG
+                Version = "v0.1.1";
+            #else
+                UpdateManager updateManager = new UpdateManager(@"D:\WinFred\Project\winfred\Releases");
+                Version version = updateManager.CurrentlyInstalledVersion();
+                Version = string.Format("v{0}.{1}.{2}", version.Major, version.Revision, version.Minor); //Todo isn't working; has to be fixed in the future
+            #endif
+            versionLabel.Content = Version;
         }
     }
 }
