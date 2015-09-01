b543d5980e98e476e65c84d5a6aecf12b8570cdc
Michael Moser <michaelmoser1996@gmail.com>
2015-09-01 11:11:08 +0200

    HotKey cleanup

--- WinFred/HotKey.cs
+++ WinFred/HotKey.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows.Input;
 using System.Windows.Interop;
@@ -12,7 +11,7 @@ namespace WinFred
         public const int WmHotKey = 0x0312;
         private static Dictionary<int, HotKey> _dictHotKeyToCalBackProc;
         private bool _disposed;
-        // ******************************************************************
+        
         public HotKey(Key k, KeyModifier keyModifiers, Action<HotKey> action, bool register = true)
         {
             Key = k;
@@ -28,18 +27,10 @@ namespace WinFred
         public KeyModifier KeyModifiers { get; }
         public Action<HotKey> Action { get; }
         public int Id { get; set; }
-        // ******************************************************************
-        // Implement IDisposable.
-        // Do not make this method virtual.
-        // A derived class should not be able to override this method.
+        
         public void Dispose()
         {
             Dispose(true);
-            // This object will be cleaned up by the Dispose method.
-            // Therefore, you should call GC.SupressFinalize to
-            // take this object off the finalization queue
-            // and prevent finalization code for this object
-            // from executing a second time.
             GC.SuppressFinalize(this);
         }
 
@@ -49,26 +40,20 @@ namespace WinFred
         [DllImport("user32.dll")]
         private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
-        // ******************************************************************
         public bool Register()
         {
             var virtualKeyCode = KeyInterop.VirtualKeyFromKey(Key);
             Id = virtualKeyCode + ((int) KeyModifiers*0x10000);
             var result = RegisterHotKey(IntPtr.Zero, Id, (uint) KeyModifiers, (uint) virtualKeyCode);
-
             if (_dictHotKeyToCalBackProc == null)
             {
                 _dictHotKeyToCalBackProc = new Dictionary<int, HotKey>();
                 ComponentDispatcher.ThreadFilterMessage += ComponentDispatcherThreadFilterMessage;
             }
-
             _dictHotKeyToCalBackProc.Add(Id, this);
-
-            Debug.Print(result + ", " + Id + ", " + virtualKeyCode);
             return result;
         }
-
-        // ******************************************************************
+        
         public void Unregister()
         {
             HotKey hotKey;
@@ -77,56 +62,33 @@ namespace WinFred
                 UnregisterHotKey(IntPtr.Zero, Id);
             }
         }
-
-        // ******************************************************************
+        
         private static void ComponentDispatcherThreadFilterMessage(ref MSG msg, ref bool handled)
         {
             if (!handled)
             {
-                if (msg.message == WmHotKey)
+                HotKey hotKey;
+                if (msg.message == WmHotKey && _dictHotKeyToCalBackProc.TryGetValue((int) msg.wParam, out hotKey))
                 {
-                    HotKey hotKey;
-
-                    if (_dictHotKeyToCalBackProc.TryGetValue((int) msg.wParam, out hotKey))
-                    {
-                        if (hotKey.Action != null)
-                        {
-                            hotKey.Action.Invoke(hotKey);
-                        }
-                        handled = true;
-                    }
+                    hotKey.Action?.Invoke(hotKey);
+                    handled = true;
                 }
             }
         }
 
-        // ******************************************************************
-        // Dispose(bool disposing) executes in two distinct scenarios.
-        // If disposing equals true, the method has been called directly
-        // or indirectly by a user's code. Managed and unmanaged resources
-        // can be _disposed.
-        // If disposing equals false, the method has been called by the
-        // runtime from inside the finalizer and you should not reference
-        // other objects. Only unmanaged resources can be _disposed.
         protected virtual void Dispose(bool disposing)
         {
-            // Check to see if Dispose has already been called.
             if (!_disposed)
             {
-                // If disposing equals true, dispose all managed
-                // and unmanaged resources.
                 if (disposing)
                 {
-                    // Dispose managed resources.
                     Unregister();
                 }
-
-                // Note disposing has been done.
                 _disposed = true;
             }
         }
     }
-
-    // ******************************************************************
+    
     [Flags]
     public enum KeyModifier
     {
@@ -137,6 +99,4 @@ namespace WinFred
         Shift = 0x0004,
         Win = 0x0008
     }
-
-    // ******************************************************************
 }
