62adfc69d2f23810d1b38142fc093ba1be2cf0d0
Michael Moser <michaelmoser1996@gmail.com>
2015-09-05 20:34:21 +0200

    Added button and their functionality
    Added button for uninstall program, check for updates, close program
    Implemented uninstall program and close program
    Disabled buttons, which aren't implemented now

--- WinFred/UserControls/GeneralUserControl.xaml.cs
+++ WinFred/UserControls/GeneralUserControl.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 using WinFred.Enumerations;
 
@@ -14,6 +16,16 @@ namespace WinFred.UserControls
             InitializeComponent();
             DataContext = Config.GetInstance();
             AccentColorComboBox.ItemsSource = Enum.GetNames(typeof (AccentColorTypes));
+        }
+
+        private void UninstallProgram(object sender, RoutedEventArgs e)
+        {
+            Process.Start(Environment.SystemDirectory + @"\appwiz.cpl");
+        }
+
+        private void CloseProgram(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
