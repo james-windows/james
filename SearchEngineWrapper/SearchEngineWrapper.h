99b9f4720302bfeaa76554ed8d14ee80bf22c036
Michael Moser <michaelmoser1996@gmail.com>
2015-09-25 17:18:26 +0200

    added SearchEngine and the SearchEngineWrapper project

---
+++ SearchEngineWrapper/SearchEngineWrapper.h
@@ -0,0 +1,13 @@
+// SearchEngineWrapper.h
+
+#pragma once
+
+using namespace System;
+
+namespace SearchEngineWrapper {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
