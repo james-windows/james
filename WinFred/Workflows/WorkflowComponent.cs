d2a30b0102ed69dbb6f310d85437c9b30cc4ff67
Michael Moser <michaelmoser1996@gmail.com>
2015-10-24 10:10:17 +0200

    ADD added a description field inside the component
    Now an additional field description will be displayed. Each Component has
    to override it at his own in the new futur (descriptions has to be
    written)

--- WinFred/Workflows/WorkflowComponent.cs
+++ WinFred/Workflows/WorkflowComponent.cs
@@ -20,7 +20,9 @@ namespace James.Workflows
         public FrameworkElement UiElement { get; set; }
         public string Name => Regex.Replace(GetType().Name, "(?<=[a-z])([A-Z])", " $1", RegexOptions.Compiled);
         public string Summary => GetSummary();
+        public string Description => GetDescription();
         public abstract string GetSummary();
+        public string GetDescription() => "A description for each Workflow component will be placed here soon! Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
 
         [DataMember]
         public Workflow ParentWorkflow { get; set; }
