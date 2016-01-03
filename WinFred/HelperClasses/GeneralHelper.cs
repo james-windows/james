bbc2939cbcfcdfbd6640048fe17cd32bd5b58dc7
Michael Moser <michaelmoser1996@gmail.com>
2016-01-03 22:04:15 +0100

    CLU GeneralHelper
    Removed code which was no longer necessary

--- WinFred/HelperClasses/GeneralHelper.cs
+++ WinFred/HelperClasses/GeneralHelper.cs
@@ -68,39 +68,5 @@ namespace James.HelperClasses
             Console.WriteLine((DateTime.Now - tmp).TotalMilliseconds);
             return imageSource;
         }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct SHFILEINFO
-        {
-            public IntPtr hIcon;
-            public IntPtr iIcon;
-            public uint dwAttributes;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
-            public string szDisplayName;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
-            public string szTypeName;
-        };
-
-
-        public const uint SHGFI_ICON = 0x100;
-        public const uint SHGFI_LARGEICON = 0x0; // 'Large icon
-        public const uint SHGFI_SMALLICON = 0x1; // 'Small icon
-
-        [DllImport("shell32.dll")]
-        public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
-
-        public static ImageSource GetIcon(string strPath)
-        {
-            SHFILEINFO shinfo = new SHFILEINFO();
-
-            SHGetFileInfo(strPath, 0, ref shinfo, (uint)Marshal.SizeOf(shinfo), SHGFI_ICON | SHGFI_LARGEICON);
-
-            if (shinfo.hIcon.ToInt32() != 0)
-            {
-                Icon myIcon = Icon.FromHandle(shinfo.hIcon);
-                return ToImageSource(myIcon);
-            }
-            return null;
-        }
     }
 }
