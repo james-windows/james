037c701c6cb8dbbdaaa9724bac09675e244f8c31
Michael Moser <michaelmoser1996@gmail.com>
2016-01-28 07:52:20 +0100

    ADD SearchBoxOutput

---
+++ WinFred/Workflows/Outputs/SearchBoxOutput.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Mime;
+using System.Windows;
+using System.Windows.Threading;
+using James.Windows;
+
+namespace James.Workflows.Outputs
+{
+    public class SearchBoxOutput: BasicOutput
+    {
+        public override void Run(string[] input)
+        {
+            string output = FormatStringToText(FormatString, input);
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                var box = MainWindow.GetInstance().SearchTextBox;
+                box.Text = output;
+                box.SelectionStart = output.Length;
+            }));          
+        }
+    }
+}
