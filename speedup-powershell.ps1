f6cb5f57499c489c2d84b97e523ca3274d875447
Michael Moser <michaelmoser1996@gmail.com>
2016-02-10 18:07:13 +0100

    Add powershell script for speeding up launch ;)

---
+++ speedup-powershell.ps1
@@ -0,0 +1,14 @@
+﻿Set-Alias ngen (Join-Path ([System.Runtime.InteropServices.RuntimeEnvironment]::GetRuntimeDirectory()) ngen.exe) 
+[AppDomain]::CurrentDomain.GetAssemblies() | 
+sort {Split-path $_.location -leaf} | 
+%{ 
+$Name = (Split-Path $_.location -leaf) 
+if ([System.Runtime.InteropServices.RuntimeEnvironment]::FromGlobalAccessCache($_)) 
+{ 
+Write-Host "Already GACed: $Name" 
+}else 
+{ 
+Write-Host -ForegroundColor Yellow "NGENing : $Name" 
+ngen install $_.location | %{"`t$_"} 
+} 
+}
