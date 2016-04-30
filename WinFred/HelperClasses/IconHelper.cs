7f7e9159145d04a7e1f5354f5ba2962ffe1aa56e
Michael Moser <michaelmoser1996@gmail.com>
2016-04-30 10:45:07 +0200

    FIX finding the path to the icons

--- WinFred/HelperClasses/IconHelper.cs
+++ WinFred/HelperClasses/IconHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Windows.Media.Imaging;
 using Alphaleonis.Win32.Filesystem;
 
@@ -6,11 +7,29 @@ namespace James.HelperClasses
 {
     public static class IconHelper
     {
+        /// <summary>
+        /// Returns the icon from the james \Resources folder
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
         public static BitmapImage GetIcon(string path)
         {
-            BitmapImage tmp = new BitmapImage(new Uri(Directory.GetCurrentDirectory() + "\\Resources\\" + path));
-            tmp.Freeze();
-            return tmp;
+            try
+            {
+                BitmapImage tmp = new BitmapImage(new Uri(GetExecuteablePath() + "\\Resources\\" + path));
+                tmp.Freeze();
+                return tmp;
+            }
+            catch (Exception)
+            {
+                return new BitmapImage();
+            }
+        }
+
+        private static string GetExecuteablePath()
+        {
+            var folders = Directory.GetDirectories(Config.ConfigFolderLocation).Where(s => PathHelper.GetFilename(s).StartsWith("app-")).Select(s => new  { path = s,version= new Version(PathHelper.GetFilename(s).Replace("app-", ""))});
+            return folders.OrderByDescending(arg => arg.version).First().path;
         }
     }
 }
